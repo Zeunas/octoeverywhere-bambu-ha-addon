22c3fb8c884b3f82764f1d7082ae47e17935757f
Zeunas <71456261+Zeunas@users.noreply.github.com>
2025-08-20 14:51:50 +0100

    Update run.sh

--- octoeverywhere-bambu/run.sh
+++ octoeverywhere-bambu/run.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bashio
-# Run OctoEverywhere Bambu for a single printer
-
 set -e
 
-# Export environment variables from HAOS options
+# Export environment variables exactly as OctoEverywhere expects
 export COMPANION_MODE="$(bashio::config 'COMPANION_MODE')"
 export PRINTER_IP="$(bashio::config 'PRINTER_IP')"
 export ACCESS_CODE="$(bashio::config 'ACCESS_CODE')"
@@ -11,9 +9,5 @@ export SERIAL_NUMBER="$(bashio::config 'SERIAL_NUMBER')"
 
 echo "Starting OctoEverywhere for printer at $PRINTER_IP..."
 
-# Run the OctoEverywhere container entrypoint
-exec /entrypoint.sh \
-    --mode "$COMPANION_MODE" \
-    --ip "$PRINTER_IP" \
-    --access "$ACCESS_CODE" \
-    --serial "$SERIAL_NUMBER"
+# Run the OctoEverywhere entrypoint
+exec /entrypoint.sh
