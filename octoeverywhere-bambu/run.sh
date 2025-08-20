743e25241e68c091c60c07209f301914161b2745
Zeunas <71456261+Zeunas@users.noreply.github.com>
2025-08-20 17:29:11 +0100

    Update run.sh

--- octoeverywhere-bambu/run.sh
+++ octoeverywhere-bambu/run.sh
@@ -2,10 +2,10 @@
 set -e
 
 # Export environment variables exactly as docker-compose expects
-export - COMPANION_MODE=$(bashio::config 'COMPANION_MODE')
-export - PRINTER_IP=$(bashio::config 'PRINTER_IP')
-export - ACCESS_CODE=$(bashio::config 'ACCESS_CODE')
-export - SERIAL_NUMBER=$(bashio::config 'SERIAL_NUMBER')
+export COMPANION_MODE=$(bashio::config 'COMPANION_MODE')
+export PRINTER_IP=$(bashio::config 'PRINTER_IP')
+export ACCESS_CODE=$(bashio::config 'ACCESS_CODE')
+export SERIAL_NUMBER=$(bashio::config 'SERIAL_NUMBER')
 
 echo "================ DEBUG ENVIRONMENT ================="
 echo "COMPANION_MODE: $COMPANION_MODE"
