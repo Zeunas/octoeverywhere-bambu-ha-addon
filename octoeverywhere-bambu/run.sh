bf7417c85076605963fec47f2f27e4b634ebae64
Zeunas <71456261+Zeunas@users.noreply.github.com>
2025-08-20 22:49:23 +0100

    Update run.sh

--- octoeverywhere-bambu/run.sh
+++ octoeverywhere-bambu/run.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
-set -e
-
-echo "[INFO] Starting OctoEverywhere Companion..."
-docker run \
-  --rm \
-  --name octoeverywhere_companion \
-  -e COMPANION_MODE="${COMPANION_MODE}" \
-  -e ACCESS_CODE="${ACCESS_CODE}" \
-  -e SERIAL_NUMBER="${SERIAL_NUMBER}" \
-  -e PRINTER_IP="${PRINTER_IP}" \
-  -v /config/octoeverywhere-bambu:/data \
-  octoeverywhere/octoeverywhere:latest
+exec /entrypoint.sh \
+  --mode "${COMPANION_MODE}" \
+  --ip "${PRINTER_IP}" \
+  --access "${ACCESS_CODE}" \
+  --serial "${SERIAL_NUMBER}"
