ab63889fd3d22b4f48fa67c42609fda69d6fe252
Zeunas <71456261+Zeunas@users.noreply.github.com>
2025-08-21 09:20:57 +0100

    Update run.sh

--- octoeverywhere-bambu/run.sh
+++ octoeverywhere-bambu/run.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env sh
 set -e
 
-# Dump all environment variables for debugging
-echo "=============== FULL ENVIRONMENT ==============="
-env
-echo "================================================"
-
-# Print only OctoEverywhere relevant variables
+# Print environment variables for debugging
 echo "=============== OctoEverywhere ENV ==============="
 echo "COMPANION_MODE=${COMPANION_MODE}"
 echo "PRINTER_IP=${PRINTER_IP}"
